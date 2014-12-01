ac2bd4d41fad57853909dddfc02c4e77e9ef3651
Heng Li <lh3@me.com>
2014-12-01 12:45:11 -0500

    query, dump and restore the hash table

--- bfc.c
+++ bfc.c
@@ -303,6 +303,21 @@ void bfc_ch_insert(bfc_ch_t *ch, uint64_t x[2], int low_flag)
 	__sync_lock_release(&h->lock); // unlock
 }
 
+int bfc_ch_get(const bfc_ch_t *ch, const bfc_kmer_t *z)
+{
+	int k = ch->k, t = !(z->x[0] + z->x[1] < z->x[2] + z->x[3]);
+	uint64_t mask = (1ULL<<k) - 1, x[2], key;
+	cnthash_t *h;
+	khint_t itr;
+
+	x[0] = bfc_hash_64(z->x[t<<1|0], mask);
+	x[1] = bfc_hash_64(z->x[t<<1|1], mask);
+	h = ch->h[x[0] & ((1ULL<<ch->l_pre) - 1)];
+	key = (x[0] >> ch->l_pre | x[1] << (ch->k - ch->l_pre)) << 14 | 1;
+	itr = kh_get(cnt, h, key);
+	return itr != kh_end(h)? kh_key(h, itr) & 0x3fff : -1;
+}
+
 uint64_t bfc_ch_count(const bfc_ch_t *ch)
 {
 	int i;
@@ -310,6 +325,90 @@ uint64_t bfc_ch_count(const bfc_ch_t *ch)
 	for (i = 0; i < 1<<ch->l_pre; ++i)
 		cnt += kh_size(ch->h[i]);
 	return cnt;
+}
+
+int bfc_ch_dump(const bfc_ch_t *ch, const char *fn)
+{
+	FILE *fp;
+	uint32_t t[2];
+	int i;
+	if ((fp = strcmp(fn, "-")? fopen(fn, "wb") : stdout) == 0) return -1;
+	t[0] = ch->k, t[1] = ch->l_pre;
+	fwrite(t, 4, 2, fp);
+	for (i = 0; i < 1<<ch->l_pre; ++i) {
+		cnthash_t *h = ch->h[i];
+		khint_t k;
+		t[0] = kh_n_buckets(h), t[1] = kh_size(h);
+		fwrite(t, 4, 2, fp);
+		for (k = 0; k < kh_end(h); ++k)
+			if (kh_exist(h, k))
+				fwrite(&kh_key(h, k), 8, 1, fp);
+	}
+	fprintf(stderr, "[M::%s] dumpped the hash table to file '%s'.\n", __func__, fn);
+	fclose(fp);
+	return 0;
+}
+
+bfc_ch_t *bfc_ch_restore(const char *fn)
+{
+	FILE *fp;
+	uint32_t t[2];
+	int i, j, absent;
+	bfc_ch_t *ch;
+
+	if ((fp = fopen(fn, "rb")) == 0) return 0;
+	fread(t, 4, 2, fp);
+	ch = bfc_ch_init(t[0]);
+	assert((int)t[1] == ch->l_pre);
+	for (i = 0; i < 1<<ch->l_pre; ++i) {
+		cnthash_t *h = ch->h[i];
+		fread(t, 4, 2, fp);
+		kh_resize(cnt, h, t[0]);
+		for (j = 0; j < t[1]; ++j) {
+			uint64_t key;
+			fread(&key, 8, 1, fp);
+			kh_put(cnt, h, key, &absent);
+			assert(absent);
+		}
+	}
+	fclose(fp);
+	fprintf(stderr, "[M::%s] restored the hash table to file '%s'.\n", __func__, fn);
+	return ch;
+}
+
+void _bfc_ch_print(const bfc_ch_t *ch)
+{
+	int i;
+	for (i = 0; i < 1<<ch->l_pre; ++i) {
+		cnthash_t *h = ch->h[i];
+		khint_t k;
+		for (k = 0; k < kh_end(h); ++k)
+			if (kh_exist(h, k))
+				printf("%d\t%d\t%d\n", (int)(kh_key(h, k)&0xff), (int)(kh_key(h,k)>>8&7), (int)(kh_key(h,k)>>11&7));
+	}
+}
+
+void bfc_ch_print_kcov(const bfc_ch_t *ch, const char *seq)
+{
+	int len, i, l;
+	uint64_t mask = (1ULL<<ch->k) - 1;
+	len = strlen(seq);
+	bfc_kmer_t x = bfc_kmer_null;
+	printf("%d\n", len);
+	for (i = l = 0; i < len; ++i) {
+		int c = seq_nt6_table[(uint8_t)seq[i]] - 1;
+		if (c < 4) {
+			x.x[0] = (x.x[0]<<1 | (c&1))  & mask;
+			x.x[1] = (x.x[1]<<1 | (c>>1)) & mask;
+			x.x[2] = x.x[2]>>1 | (1ULL^(c&1))<<(ch->k-1);
+			x.x[3] = x.x[3]>>1 | (1ULL^c>>1) <<(ch->k-1);
+			if (++l >= ch->k) {
+				int r;
+				r = bfc_ch_get(ch, &x);
+				if (r >= 0) fprintf(stderr, "%d\t%d\t%d\t%d\t%d\n", i - ch->k + 1, i + 1, r&0xff, r>>8&7, r>>11&7);
+			}
+		} else l = 0, x = bfc_kmer_null;
+	}
 }
 
 /**********************
@@ -390,7 +489,8 @@ void *bfc_count_cb(void *shared, int step, void *_data)
 		int i;
 		bfc_count_data_t *data = (bfc_count_data_t*)_data;
 		kt_for(aux->opt.n_threads, worker, data, data->n_seqs);
-		fprintf(stderr, "[M::%s] processed %d sequences (CPU/real time: %.3f/%.3f secs)\n", __func__, data->n_seqs, cputime(), realtime() - bfc_real_time);
+		fprintf(stderr, "[M::%s] processed %d sequences (CPU/real time: %.3f/%.3f secs; # distinct k-mers: %ld)\n",
+				__func__, data->n_seqs, cputime(), realtime() - bfc_real_time, (long)bfc_ch_count(aux->ch));
 		for (i = 0; i < data->n_seqs; ++i) {
 			free(data->seqs[i].seq); free(data->seqs[i].qual);
 		}
@@ -404,11 +504,15 @@ int main(int argc, char *argv[])
 	gzFile fp;
 	bfc_aux_t aux;
 	int i, c, no_mt_io = 0;
+	char *in_hash = 0, *out_hash = 0, *str_kcov = 0;
 
 	bfc_real_time = realtime();
 	bfc_opt_init(&aux.opt);
-	while ((c = getopt(argc, argv, "k:s:b:L:t:h:q:J")) >= 0) {
+	while ((c = getopt(argc, argv, "d:k:s:b:L:t:C:h:q:Jr:")) >= 0) {
 		if (c == 'k') aux.opt.k = atoi(optarg);
+		else if (c == 'C') str_kcov = optarg;
+		else if (c == 'd') out_hash = optarg;
+		else if (c == 'r') in_hash = optarg;
 		else if (c == 'q') aux.opt.q = atoi(optarg);
 		else if (c == 'b') aux.opt.n_shift = atoi(optarg);
 		else if (c == 't') aux.opt.n_threads = atoi(optarg);
@@ -437,19 +541,28 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "         -t INT       number of threads [%d]\n", aux.opt.n_threads);
 		fprintf(stderr, "         -b INT       set Bloom Filter size to pow(2,INT) bits [%d]\n", aux.opt.n_shift);
 		fprintf(stderr, "         -h INT       use INT hash functions for Bloom Filter [%d]\n", aux.opt.n_hashes);
+		fprintf(stderr, "         -d FILE      dump hash table to FILE [null]\n");
+		fprintf(stderr, "         -r FILE      restore hash table from FILE [null]\n");
 		fprintf(stderr, "\n");
 		return 1;
 	}
 
-	aux.bf = bfc_bf_init(aux.opt.n_shift, aux.opt.n_hashes);
-	aux.ch = bfc_ch_init(aux.opt.k);
+	if (in_hash == 0) {
+		aux.bf = bfc_bf_init(aux.opt.n_shift, aux.opt.n_hashes);
+		aux.ch = bfc_ch_init(aux.opt.k);
 
-	fp = strcmp(argv[optind], "-")? gzopen(argv[optind], "r") : gzdopen(fileno(stdin), "r");
-	aux.ks = kseq_init(fp);
-	kt_pipeline(no_mt_io? 1 : 2, bfc_count_cb, &aux, 2);
-	kseq_destroy(aux.ks);
-	gzclose(fp);
+		fp = strcmp(argv[optind], "-")? gzopen(argv[optind], "r") : gzdopen(fileno(stdin), "r");
+		aux.ks = kseq_init(fp);
+		kt_pipeline(no_mt_io? 1 : 2, bfc_count_cb, &aux, 2);
+		kseq_destroy(aux.ks);
+		gzclose(fp);
 
+		bfc_bf_destroy(aux.bf);
+		aux.bf = 0;
+	} else aux.ch = bfc_ch_restore(in_hash);
+
+	if (str_kcov) bfc_ch_print_kcov(aux.ch, str_kcov);
+	if (out_hash) bfc_ch_dump(aux.ch, out_hash);
 	bfc_ch_destroy(aux.ch);
 	bfc_bf_destroy(aux.bf);
 
