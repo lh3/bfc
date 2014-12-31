f297356e16c0e42e062ded87b44620a666e9c901
Heng Li <lh3@me.com>
2014-12-31 15:17:33 -0500

    forgot to add the two files

---
+++ bseq.h
@@ -0,0 +1,19 @@
+#ifndef BFC_BSEQ_H
+#define BFC_BSEQ_H
+
+struct bseq_file_s;
+typedef struct bseq_file_s bseq_file_t;
+
+typedef struct {
+	int l_seq;
+	uint32_t aux;
+	char *name, *seq, *qual;
+} bseq1_t;
+
+extern unsigned char seq_nt6_table[256];
+
+bseq_file_t *bseq_open(const char *fn);
+void bseq_close(bseq_file_t *fp);
+bseq1_t *bseq_read(bseq_file_t *fp, int chunk_size, int *n_);
+
+#endif
