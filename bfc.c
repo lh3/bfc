9c05655cee80c4169aff4cd87fa116725ca14939
Heng Li <lh3@me.com>
2014-12-31 14:23:59 -0500

    r72: optionally drop bad reads

--- bfc.c
+++ bfc.c
@@ -5,7 +5,7 @@
 #include <assert.h>
 #include <limits.h>
 
-#define BFC_VERSION "r71"
+#define BFC_VERSION "r72"
 
 /******************
  * Hash functions *
@@ -121,6 +121,8 @@ typedef struct {
 
 	int w_ec, w_ec_high, w_absent, w_absent_high;
 	int max_path_diff;
+
+	int discard;
 } bfc_opt_t;
 
 void bfc_opt_init(bfc_opt_t *opt)
@@ -1033,6 +1035,7 @@ void *bfc_ec_cb(void *shared, int step, void *_data)
 		int i;
 		for (i = 0; i < data->n_seqs; ++i) {
 			bseq1_t *s = &data->seqs[i];
+			if (aux->opt->discard && (s->aux&1)) continue;
 			printf("%c%s\tec:Z:%c_%d_%d\n%s\n", s->qual? '@' : '>', s->name,
 				   "TF"[s->aux&1], s->aux>>16&0xffff, s->aux>>1&0x7fff, s->seq);
 			if (s->qual) printf("+\n%s\n", s->qual);
@@ -1061,6 +1064,7 @@ static void usage(FILE *fp, bfc_opt_t *o)
 	fprintf(fp, "  -r FILE      restore hash table from FILE [null]\n");
 	fprintf(fp, "  -w INT       no more than %d ec or 2 highQ ec in INT-bp window [%d]\n", BFC_EC_HIST, o->win_multi_ec);
 	fprintf(fp, "  -c INT       min k-mer coverage [%d]\n", o->min_cov);
+	fprintf(fp, "  -D           discard uncorrectable reads\n");
 	fprintf(fp, "  -v           show version number\n");
 	fprintf(fp, "  -h           show command line help\n");
 }
@@ -1078,7 +1082,7 @@ int main(int argc, char *argv[])
 	bfc_real_time = realtime();
 	bfc_opt_init(&opt);
 	caux.opt = &opt;
-	while ((c = getopt(argc, argv, "hvV:Ed:k:s:b:L:t:C:H:q:Jr:c:w:")) >= 0) {
+	while ((c = getopt(argc, argv, "hvV:Ed:k:s:b:L:t:C:H:q:Jr:c:w:D")) >= 0) {
 		if (c == 'k') opt.k = atoi(optarg);
 		else if (c == 'C') str_kcov = optarg;
 		else if (c == 'd') out_hash = optarg;
@@ -1089,6 +1093,7 @@ int main(int argc, char *argv[])
 		else if (c == 'H') opt.n_hashes = atoi(optarg);
 		else if (c == 'c') opt.min_cov = atoi(optarg);
 		else if (c == 'w') opt.win_multi_ec = atoi(optarg);
+		else if (c == 'D') opt.discard = 1;
 		else if (c == 'J') no_mt_io = 1; // for debugging kt_pipeline()
 		else if (c == 'E') no_ec = 1;
 		else if (c == 'V') bfc_verbose = atoi(optarg);
