f7020bc885957d4beb70f9f99438956f2b6e9c40
Heng Li <lh3@me.com>
2015-01-07 13:31:27 -0500

    restructured for pair comparison in future

--- errstat.js
+++ errstat.js
@@ -1,49 +1,90 @@
-var file = arguments.length? new File(arguments[0]) : new File();
-var buf = new Bytes();
+// Usage: k8 errstat.js [ec1.sam] [ec2.sam]
+
 var re = /(\d+)([MIDNSH])/g;
 
-var n_err_bases = 0, n_err_reads = 0, tot_reads = 0, n_chimeric = 0, n_chimeric_reads = 0, n_unmapped = 0, n_perfect = 0, n_clipped = 0, tot_clip = 0;
-var last = null, last_nm = 0, last_seg = 0, last_clip = 0;
-
-function count(last_nm, last_seg, last_clip)
+function read1(f, b, t)
 {
-	++tot_reads;
-	tot_clip += last_clip;
-	if (last_nm == 0 && last_clip == 0 && last_seg == 1) ++n_perfect;
-	if (last_nm > 0) ++n_err_reads, n_err_bases += last_nm;
-	if (last_clip != 0) ++n_clipped;
-	if (last_seg == 0) ++n_unmapped;
-	else if (last_seg > 1) ++n_chimeric_reads, n_chimeric += last_seg - 1;
-}
+	var st = {}, lines = [], m, name;
 
-while (file.readline(buf) >= 0) {
-	var line = buf.toString();
-	if (line.charAt(0) == '@') continue;
-	var m, t = line.split("\t");
-	var flag = parseInt(t[1]), nm = 0;
-	var seg = (flag&4)? 0 : 1;
-	if ((m = /NM:i:(\d+)/.exec(line)) != null)
-		nm = parseInt(m[1]);
-	var name = t[0] + '/' + (flag>>6&3);
-	if (name != last) {
-		if (last) count(last_nm, last_seg, last_clip);
-		last = name, last_nm = nm, last_seg = seg, last_clip = 0;
+	if (t == null) {
+		while (f.readline(b) >= 0) {
+			if (b[0] == 64) continue;
+			t = b.toString().split("\t");
+			break;
+		}
+		if (t == null) return null; // end of file
+		t[1] = parseInt(t[1]);
+	}
+	name = t[0] + '/' + (t[1]>>6&3);
+	lines.push(t);
+	t = null;
+
+	while (f.readline(b) >= 0) {
+		t = b.toString().split("\t");
+		t[1] = parseInt(t[1]);
+		var s = t[0] + '/' + (t[1]>>6&3);
+		if (s != name) break;
+		lines.push(t);
+	}
+
+	st.next = t;
+	st.n_segs = 0;
+	st.nm = 0;
+	st.cliplen = 0;
+	t = lines[0];
+	if ((t[1]&4) == 0) {
 		while ((m = re.exec(t[5])) != null)
 			if (m[2] == 'S' || m[2] == 'H')
-				last_clip += parseInt(m[1]);
-	} else last_seg += seg, last_nm += nm;
+				st.cliplen += parseInt(m[1]);
+	}
+	for (var i = 0; i < lines.length; ++i) {
+		t = lines[i];
+		if (t[1]&4) continue;
+		for (var j = 11; j < t.length; ++j)
+			if (t[j].substr(0, 5) == "NM:i:")
+				st.nm += parseInt(t[j].substr(5));
+		++st.n_segs;
+	}
+	return st;
 }
-if (last) count(last_nm, last_seg, last_clip);
 
-buf.destroy();
-file.close();
+if (arguments.length < 2) {
+	var file = arguments.length? new File(arguments[0]) : new File();
+	var buf = new Bytes();
+	var st;
 
-print("# reads:             " + tot_reads);
-print("# perfect reads:     " + n_perfect);
-print("# unmapped reads:    " + n_unmapped);
-print("# chimeric reads:    " + n_chimeric_reads);
-print("# chimeric events:   " + n_chimeric);
-print("# reads w/ base err: " + n_err_reads);
-print("# error bases:       " + n_err_bases);
-print("# clipped reads:     " + n_clipped);
-print("# clipped bases:     " + tot_clip);
+	var n_err_bases = 0, n_err_reads = 0, tot_reads = 0, n_chimeric = 0, n_chimeric_reads = 0, n_unmapped = 0, n_perfect = 0, n_clipped = 0, tot_clip = 0;
+	var last = null, st;
+
+	while ((st = read1(file, buf, last)) != null) {
+		++tot_reads;
+		tot_clip += st.cliplen;
+		if (st.nm == 0 && st.cliplen == 0 && st.n_segs == 1) ++n_perfect;
+		if (st.nm > 0) ++n_err_reads, n_err_bases += st.nm;
+		if (st.cliplen != 0) ++n_clipped;
+		if (st.n_segs == 0) ++n_unmapped;
+		else if (st.n_segs > 1) ++n_chimeric_reads, n_chimeric += st.n_segs - 1;
+		last = st.next;
+	}
+
+	buf.destroy();
+	file.close();
+
+	print("# reads:             " + tot_reads);
+	print("# perfect reads:     " + n_perfect);
+	print("# unmapped reads:    " + n_unmapped);
+	print("# chimeric reads:    " + n_chimeric_reads);
+	print("# chimeric events:   " + n_chimeric);
+	print("# reads w/ base err: " + n_err_reads);
+	print("# error bases:       " + n_err_bases);
+	print("# clipped reads:     " + n_clipped);
+	print("# clipped bases:     " + tot_clip);
+} else {
+	var f1 = new File(arguments[0]);
+	var f2 = new File(arguments[1]);
+	var buf = new Bytes();
+
+	buf.destroy();
+	f2.close();
+	f1.close();
+}
