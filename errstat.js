3eb23e29c165da77616727aa2614855f41861e65
Heng Li <lh3@me.com>
2015-01-07 15:47:04 -0500

    merged single/pair file stats

--- errstat.js
+++ errstat.js
@@ -45,45 +45,24 @@ function read1(f, b, t)
 	return st;
 }
 
-if (arguments.length < 2) {
-	var file = arguments.length? new File(arguments[0]) : new File();
-	var buf = new Bytes();
-	var st;
+var f1 = arguments.length? new File(arguments[0]) : new File();
+var f2 = arguments.length >= 2? new File(arguments[1]) : null;
+var buf = new Bytes();
+var st;
 
-	var n_err_bases = 0, n_err_reads = 0, tot_reads = 0, n_chimeric = 0, n_chimeric_reads = 0, n_unmapped = 0, n_perfect = 0, n_clipped = 0, tot_clip = 0;
-	var last = null, st;
+var n_err_bases = 0, n_err_reads = 0, tot_reads = 0, n_chimeric = 0, n_chimeric_reads = 0, n_unmapped = 0, n_perfect = 0, n_clipped = 0, tot_clip = 0;
+var n1 = 0, n2 = 0, na = 0;
+var st1, st2, last1 = null, last2 = null;
 
-	while ((st = read1(file, buf, last)) != null) {
-		++tot_reads;
-		tot_clip += st.cliplen;
-		if (st.nm == 0 && st.cliplen == 0 && st.n_segs == 1) ++n_perfect;
-		if (st.nm > 0) ++n_err_reads, n_err_bases += st.nm;
-		if (st.cliplen != 0) ++n_clipped;
-		if (st.n_segs == 0) ++n_unmapped;
-		else if (st.n_segs > 1) ++n_chimeric_reads, n_chimeric += st.n_segs - 1;
-		last = st.next;
-	}
-
-	buf.destroy();
-	file.close();
-
-	print("# reads:             " + tot_reads);
-	print("# perfect reads:     " + n_perfect);
-	print("# unmapped reads:    " + n_unmapped);
-	print("# chimeric reads:    " + n_chimeric_reads);
-	print("# chimeric events:   " + n_chimeric);
-	print("# reads w/ base err: " + n_err_reads);
-	print("# error bases:       " + n_err_bases);
-	print("# clipped reads:     " + n_clipped);
-	print("# clipped bases:     " + tot_clip);
-} else {
-	var f1 = new File(arguments[0]);
-	var f2 = new File(arguments[1]);
-	var buf = new Bytes();
-	var st1, st2, last1 = null, last2 = null;
-	var n1 = 0, n2 = 0, na = 0;
-
-	while ((st1 = read1(f1, buf, last1)) != null) {
+while ((st1 = read1(f1, buf, last1)) != null) {
+	++tot_reads;
+	tot_clip += st1.cliplen;
+	if (st1.nm == 0 && st1.cliplen == 0 && st1.n_segs == 1) ++n_perfect;
+	if (st1.nm > 0) ++n_err_reads, n_err_bases += st1.nm;
+	if (st1.cliplen != 0) ++n_clipped;
+	if (st1.n_segs == 0) ++n_unmapped;
+	else if (st1.n_segs > 1) ++n_chimeric_reads, n_chimeric += st1.n_segs - 1;
+	if (f2) {
 		st2 = read1(f2, buf, last2);
 		if (st2 == null) throw Error("the 2nd file has fewer reads");
 		//if (st1.name != st2.name) throw Error("different read names: "+st1.name+ " vs "+st2.name);
@@ -94,13 +73,28 @@ if (arguments.length < 2) {
 			} else if (st2.nm <= st1.nm && st2.cliplen <= st1.cliplen && st2.n_segs == 1) {
 				t = "2", ++n2;
 			} else t = "a", ++na;
-			print(st1.name, t, st1.n_segs, st1.cliplen, st1.nm, st2.n_segs, st2.cliplen, st2.nm);
+			print(t, st1.name, st1.n_segs, st1.cliplen, st1.nm, st2.n_segs, st2.cliplen, st2.nm);
 		}
-		last1 = st1.next, last2 = st2.next;
+		last2 = st2.next;
 	}
-
-	buf.destroy();
-	f2.close();
-	f1.close();
-	warn(n1, n2, na);
+	last1 = st1.next;
 }
+
+print("# reads:             " + tot_reads);
+print("# perfect reads:     " + n_perfect);
+print("# unmapped reads:    " + n_unmapped);
+print("# chimeric reads:    " + n_chimeric_reads);
+print("# chimeric events:   " + n_chimeric);
+print("# reads w/ base err: " + n_err_reads);
+print("# error bases:       " + n_err_bases);
+print("# clipped reads:     " + n_clipped);
+print("# clipped bases:     " + tot_clip);
+if (f2) {
+	print("# better reads:      " + n1);
+	print("# worse reads:       " + n2);
+	print("# ambiguous reads:   " + na);
+}
+
+buf.destroy();
+if (f2) f2.close();
+f1.close();
